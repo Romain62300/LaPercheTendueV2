b0ba1ace68839966602a94bbd774c000839dd0ad
Romain <r.monier62@hotmail.com>
2025-02-28 22:54:28 +0100

    Ajout du fichier modèle config.example.php

---
+++ includes/config.example.php
@@ -0,0 +1,9 @@
+<?php
+// Exemple de fichier de configuration (NE PAS LE MODIFIER DIRECTEMENT)
+// Copiez ce fichier en `config.php` et remplissez les informations
+
+define('DB_HOST', 'localhost');
+define('DB_NAME', 'nom_de_la_base');
+define('DB_USER', 'utilisateur');
+define('DB_PASS', 'mot_de_passe');
+?>
